b43c9daddc7baf6b30a99ccb7a8f2ec795da4caf
Ricardo Vitor <ricardovdev@hotmail.com>
2025-07-21 16:26:20 -0300

    adicionando o dto para criar o produto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProductDto {}
+import { IsArray, IsNotEmpty, IsNumber, IsPositive, IsString, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({ message: 'O título do produto não pode estar vazio.' })
+    @IsString()
+    title: string;
+
+    @IsNotEmpty({ message: 'A descrição do produto não pode estar vazio(a).' })
+    @IsString()
+    description: string;
+
+    @IsNotEmpty({ message: 'O preço do produto não pode estar vazio(a).' })
+    @IsNumber({ maxDecimalPlaces: 2 }, { message: 'O preço deve ser um número e só pode ter duas casas decimais.' })
+    @IsPositive({ message: 'O preço precisa ser um número positivo.' })
+    price: number;
+
+    @IsNotEmpty({ message: 'A quantidade do produto não pode estar vazio(a).' })
+    @IsNumber({}, { message: 'A quantidade precisa ser um número.' })
+    @Min(0, { message: 'A quantidade no estoque não pode ser negativo.' })
+    quantity: number;
+
+    @IsNotEmpty({ message: 'A imagem não pode estar vazio(a).' })
+    @IsArray({ message: 'Imagens deve ser salva em formato de array.' })
+    images: string[];
+
+    @IsNotEmpty({ message: 'A categoria não pode estar vazio(a).' })
+    @IsNumber({}, { message: 'Id da categoria deve ser um número.' })
+    category: number;
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ProductEntity, UserEntity])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
